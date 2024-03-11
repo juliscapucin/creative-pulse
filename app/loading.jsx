f792e4e4e765a1c2e4b4643b89bdd402661612d0
juliscapucin <jscapucin@gmail.com>
2024-03-11 12:44:00 +0100

    ui: fix loading

--- app/loading.jsx
+++ app/loading.jsx
@@ -2,7 +2,7 @@ import { Loader } from '@/components'
 
 const LoadingPage = ({ loading }) => {
   return (
-    <Loader/>
+    <div className='w-screen h-screen flex justify-center items-center'><Loader/></div>
   );
 };
 export default LoadingPage;
